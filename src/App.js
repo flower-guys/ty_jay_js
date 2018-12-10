94ed4ade03ad4442deb11e8553767eaad2456350
Jinsol Kim <jskim@gdcomm.io>
2018-12-10 09:53:42 +0900

    styled-components 추가 및 임시 페이지 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import * as firebase from 'firebase';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAl3H_ijO7wZVFV2juq1ofEWNB_euZeyQY",
@@ -13,25 +12,41 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: #cd4154;
+  a {
+    color: #FFF;
+    text-decoration: none;
+  }
+`;
+const ImgWrapper = styled.img`
+  width: 95vw;
+  max-width: 550px;
+  height: auto;
+  border-radius: 10px;
+`;
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <Container>
+        <ImgWrapper src='https://firebasestorage.googleapis.com/v0/b/ty-jay-js.appspot.com/o/KakaoTalk_Photo_2018-12-10-09-40-03.jpeg?alt=media&token=f54d4d6d-8e69-429d-a208-2779921c5979' />
+        <a
+          className="App-link"
+          href="https://www.notion.so/tyjayjs/5f4f4473147a44cb98834da5cd61c0cf?v=9f7101db210241ff845c8bcd52f50b3a"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <p>
             TY👏Jay👊JS🙌
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        </a>
+      </Container>
     );
   }
 }
